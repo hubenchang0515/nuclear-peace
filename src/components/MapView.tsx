73f98f08705b49fa8aecbc657660020feb507bf5
Plan C <hubenchang0515@outlook.com>
2025-03-19 17:40:53 +0800

    修复图层消失的问题

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -14,6 +14,7 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import { fireball, heavyBlast, nuclearRadiation, thermalRadiation } from '../libs/Nuke';
 import Stroke from 'ol/style/Stroke';
+import { wrapX } from 'ol/coordinate';
 
 export interface MapProps {
     url: string;
@@ -68,6 +69,7 @@ export default function MapView(props:MapProps) {
         if (isNaN(props.boom[0]) || isNaN(props.boom[1])) {
             return;
         }
+        const boom = wrapX(props.boom, mapRef.current?.getView().getProjection()!);
         const fireball = document.createElement('div');
         fireball.className = 'nuclear-fireball';
         const mashroom = document.createElement('div');
@@ -77,7 +79,7 @@ export default function MapView(props:MapProps) {
         const overlay = new Overlay({
             element: mashroom,
             positioning: 'center-center',
-            position: props.boom,
+            position: boom,
         });
 
         mapRef.current?.addOverlay(overlay);
@@ -94,28 +96,30 @@ export default function MapView(props:MapProps) {
             return;
         }
 
-        const thermalRadiationCircle = new Circle(props.boom, 1000*thermalRadiation(props.tnt));
+        const boom = wrapX(props.boom, mapRef.current?.getView().getProjection()!)
+
+        const thermalRadiationCircle = new Circle(boom, 1000*thermalRadiation(props.tnt));
         const thermalRadiationFeature = new Feature(thermalRadiationCircle);
         thermalRadiationFeature.setStyle(new Style({
             fill: new Fill({color: 'rgba(76,172,224, 0.2)'}),
             stroke: new Stroke({color: 'rgba(76,172,224, 1)'})
         }));
 
-        const nuclearRadiationCircle = new Circle(props.boom, 1000*nuclearRadiation(props.tnt));
+        const nuclearRadiationCircle = new Circle(boom, 1000*nuclearRadiation(props.tnt));
         const nuclearRadiationFeature = new Feature(nuclearRadiationCircle);
         nuclearRadiationFeature.setStyle(new Style({
             fill: new Fill({color: 'rgba(64,137,68, 0.2)'}),
             stroke: new Stroke({color: 'rgba(64,137,68, 1)'})
         }));
 
-        const heavyBlastCircle = new Circle(props.boom, 1000*heavyBlast(props.tnt));
+        const heavyBlastCircle = new Circle(boom, 1000*heavyBlast(props.tnt));
         const heavyBlastFeature = new Feature(heavyBlastCircle);
         heavyBlastFeature.setStyle(new Style({
             fill: new Fill({color: 'rgba(239,121,24, 0.2)'}),
             stroke: new Stroke({color: 'rgba(239,121,24, 1)'})
         }));
         
-        const fireballCircle = new Circle(props.boom, 1000*fireball(props.tnt));
+        const fireballCircle = new Circle(boom, 1000*fireball(props.tnt));
         const fireballFeature = new Feature(fireballCircle);
         fireballFeature.setStyle(new Style({
             fill: new Fill({color: 'rgba(255, 0, 0, 0.2)'}),
